37e44799ad5f6bfeef948075fc98b2fd6a889085
Zied Bouazizi <zied.bouazizi@outlook.com>
2025-10-11 17:10:49 +0100

    Create /dashboard, /surveys, /login and /signup routes

---
+++ react/src/router.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import Dashboard from "./views/Dashboard";
+import Surveys from "./views/Surveys";
+import Login from "./views/Login";
+import Signup from "./views/Signup";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Dashboard />
+    },
+    {
+        path: "/surveys",
+        element: <Surveys />
+    },
+    {
+        path: "/login",
+        element: <Login />
+    },
+    {
+        path: "/signup",
+        element: <Signup />
+    }
+]);
+
+export default router;

d5ffe1f36d2fa1f1f74f2fdf02b5de432407588f
Zied Bouazizi <zied.bouazizi@outlook.com>
2025-10-11 20:01:23 +0100

    Create default layout

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,18 +1,29 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import Dashboard from "./views/Dashboard";
 import Surveys from "./views/Surveys";
 import Login from "./views/Login";
 import Signup from "./views/Signup";
 import GuestLayout from "./components/GuestLayout";
+import DefaultLayout from "./components/DefaultLayout";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Dashboard />
-    },
-    {
-        path: "/surveys",
-        element: <Surveys />
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: "/dashboard",
+                element: <Navigate to="/" />
+            },
+            {
+                path: "/",
+                element: <Dashboard />
+            },
+            {
+                path: "/surveys",
+                element: <Surveys />
+            },
+        ]
     },
     {
         path: "/",

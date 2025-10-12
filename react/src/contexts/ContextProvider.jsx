79305cff52c041082796b8eb72ef549418c9fbf8
Zied Bouazizi <zied.bouazizi@outlook.com>
2025-10-12 12:42:57 +0100

    Cleanup DefaultLayout.jsx

--- react/src/contexts/ContextProvider.jsx
+++ react/src/contexts/ContextProvider.jsx
@@ -1,9 +1,17 @@
 import { createContext, useContext, useState } from "react";
 
-const StateContext = createContext({})
+const StateContext = createContext({
+    currentUser: {},
+    userToken: null,
+    setCurrentUser: () => {},
+    setUserToken: () => {}
+})
 
 export const ContextProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState({})
+    const [currentUser, setCurrentUser] = useState({
+        name: 'Tom Cook',
+        email: 'tom@example.com'
+    })
     const [userToken, setUserToken] = useState(null)
 
     return (

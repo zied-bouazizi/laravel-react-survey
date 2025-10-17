d4ed5b76be6f0bcf845a85649b365f6bb6932590
Zied Bouazizi <zied.bouazizi@outlook.com>
2025-10-17 17:29:19 +0100

    Implement displaying surveys from database instead of dummy surveys

--- react/src/views/Surveys.jsx
+++ react/src/views/Surveys.jsx
@@ -1,15 +1,37 @@
 import { PlusCircleIcon } from "@heroicons/react/24/outline";
 import TButton from "../components/core/TButton";
 import PageComponent from "../components/PageComponent";
-import { useStateContext } from "../contexts/ContextProvider";
 import SurveyListItem from "../components/SurveyListItem";
+import { useEffect, useState } from "react";
+import axiosClient from "../axios";
+import PaginationLinks from "../components/PaginationLinks";
 
 export default function Surveys() {
-  const { surveys } = useStateContext()
+  const [surveys, setSurveys] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [meta, setMeta] = useState({});
 
   const onDeleteClick = (ev) => {
     ev.preventDefault()
   }
+
+  const onPageClick = (link) => {
+    getSurveys(link.url);
+  };
+
+  const getSurveys = (url) => {
+    url = url || "/survey";
+    setLoading(true);
+    axiosClient.get(url).then(({ data }) => {
+      setSurveys(data.data);
+      setMeta(data.meta);
+      setLoading(false);
+    });
+  };
+
+  useEffect(() => {
+    getSurveys();
+  }, []);
 
   return (
     <PageComponent title='Surveys'
@@ -17,11 +39,23 @@ export default function Surveys() {
                      <PlusCircleIcon className="w-6 h-6 mr-2" />
                      Create new
                    </TButton>)}>
-      <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
-        {surveys.map(survey => (
-          <SurveyListItem key={survey.id} survey={survey} onDeleteClick={onDeleteClick} />
-        ))}
-      </div>
+      
+      {loading && <div className="text-center text-lg">loading...</div>}
+      {!loading && (
+        <div>
+          {surveys.length === 0 && 
+            <div className="py-8 text-center text-gray-700">
+              You don't have surveys created
+            </div>
+          }
+          <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
+            {surveys.map(survey => (
+              <SurveyListItem key={survey.id} survey={survey} onDeleteClick={onDeleteClick} />
+            ))}
+          </div>
+          {surveys.length > 0 && <PaginationLinks meta={meta} onPageClick={onPageClick} />}
+        </div>
+      )}
     </PageComponent>
   )
 }

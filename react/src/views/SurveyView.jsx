cb7c9b94bdcfdd4ad0a0a35eab4d42b3b7a39787
Zied Bouazizi <zied.bouazizi@outlook.com>
2025-10-17 18:32:01 +0100

    Implement updating survey

--- react/src/views/SurveyView.jsx
+++ react/src/views/SurveyView.jsx
@@ -1,7 +1,7 @@
 import { PhotoIcon, PlusIcon } from "@heroicons/react/24/outline";
 import PageComponent from "../components/PageComponent";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import axiosClient from "../axios.js";
 import SurveyQuestions from "../components/SurveyQuestions.jsx";
 import TButton from "../components/core/TButton.jsx";
@@ -9,6 +9,7 @@ import { v4 as uuidv4 } from "uuid";
 
 export default function SurveyView() {
     const navigate = useNavigate();
+    const { id } = useParams();
     const [survey, setSurvey] = useState({
         title: "",
         slug: "",
@@ -19,6 +20,7 @@ export default function SurveyView() {
         expire_date: "",
         questions: []
     })
+    const [loading, setLoading] = useState(false);
     const [errors, setErrors] = useState("");
 
     const onImageChoose = (ev) => {
@@ -46,8 +48,14 @@ export default function SurveyView() {
         }
         delete payload.image_url;
 
-        axiosClient.post("/survey", payload)
-            .then(() => {
+        let res = null;
+
+        if (id) {
+            res = axiosClient.put(`/survey/${id}`, payload);
+        } else {
+            res = axiosClient.post("/survey", payload);
+        }
+            res.then(() => {
                 navigate("/surveys");
             })
             .catch((err) => {
@@ -72,128 +80,142 @@ export default function SurveyView() {
         setSurvey({ ...survey });
     };
 
+    useEffect(() => {
+        if (id) {
+            setLoading(true);
+            axiosClient.get(`/survey/${id}`)
+                .then(({ data }) => {
+                    setSurvey(data.data);
+                    setLoading(false);
+                });
+        }
+    }, []);
+
     return (
-        <PageComponent title={'Create new Survey'}>
-            <form action="#" method="POST" onSubmit={onSubmit}>
-                <div className="shadow sm:overflow-hidden sm:rounded-md">
-                    <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
-                        {errors && (
-                            <div className="bg-red-500 text-white py-3 px-3">
-                                {Object.keys(errors).map(key => (
-                                    <p key={key}>{errors[key][0]}</p>
-                                ))}
+        <PageComponent title={!id ? 'Create new Survey' : 'Update Survey'}>
+            {loading && <div className="text-center text-lg">loading...</div>}
+            {!loading && 
+                <form action="#" method="POST" onSubmit={onSubmit}>
+                    <div className="shadow sm:overflow-hidden sm:rounded-md">
+                        <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
+                            {errors && (
+                                <div className="bg-red-500 text-white py-3 px-3">
+                                    {Object.keys(errors).map(key => (
+                                        <p key={key}>{errors[key][0]}</p>
+                                    ))}
+                                </div>
+                            )}
+
+                            {/*Image*/}
+                            <div>
+                            <label className="block text-sm font-medium text-gray-700">Photo</label>
+                            <div className="mt-1 flex items-center">
+                                {survey.image_url && (
+                                    <img src={survey.image_url} alt="" className="w-32 h-32 object-cover" />
+                                )}
+                                {!survey.image_url && (
+                                    <span
+                                    className="flex justify-center  items-center text-gray-400 h-12 w-12 overflow-hidden rounded-full bg-gray-100">
+                                    <PhotoIcon className="w-8 h-8"/>
+                                    </span>
+                                )}
+                                <button
+                                type="button"
+                                className="relative ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                >
+                                <input type="file" className="absolute left-0 top-0 right-0 bottom-0 opacity-0" onChange={onImageChoose}/>
+                                Change
+                                </button>
                             </div>
-                        )}
+                            </div>
+                            {/*Image*/}
 
-                        {/*Image*/}
-                        <div>
-                        <label className="block text-sm font-medium text-gray-700">Photo</label>
-                        <div className="mt-1 flex items-center">
-                            {survey.image_url && (
-                                <img src={survey.image_url} alt="" className="w-32 h-32 object-cover" />
-                            )}
-                            {!survey.image_url && (
-                                <span
-                                className="flex justify-center  items-center text-gray-400 h-12 w-12 overflow-hidden rounded-full bg-gray-100">
-                                <PhotoIcon className="w-8 h-8"/>
-                                </span>
-                            )}
-                            <button
-                            type="button"
-                            className="relative ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                            >
-                            <input type="file" className="absolute left-0 top-0 right-0 bottom-0 opacity-0" onChange={onImageChoose}/>
-                            Change
-                            </button>
-                        </div>
-                        </div>
-                        {/*Image*/}
-
-                        {/*Title*/}
-                        <div className="col-span-6 sm:col-span-3">
-                        <label htmlFor="title" className="block text-sm font-medium text-gray-700">
-                            Survey Title
-                        </label>
-                        <input
-                            type="text"
-                            name="title"
-                            id="title"
-                            value={survey.title}
-                            onChange={ev => setSurvey({...survey, title: ev.target.value})}
-                            placeholder="Survey Title"
-                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                        />
-                        </div>
-                        {/*Title*/}
-
-                        {/*Description*/}
-                        <div className="col-span-6 sm:col-span-3">
-                        <label htmlFor="description" className="block text-sm font-medium text-gray-700">
-                            Description
-                        </label>
-                        <textarea
-                            name="description"
-                            id="description"
-                            value={survey.description}
-                            onChange={ev => setSurvey({...survey, description: ev.target.value})}
-                            placeholder="Describe your survey"
-                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                        ></textarea>
-                        </div>
-                        {/*Description*/}
-
-                        {/*Expire Date*/}
-                        <div className="col-span-6 sm:col-span-3">
-                        <label htmlFor="expire_date" className="block text-sm font-medium text-gray-700">
-                            Expire Date
-                        </label>
-                        <input
-                            type="date"
-                            name="expire_date"
-                            id="expire_date"
-                            value={survey.expire_date}
-                            onChange={ev => setSurvey({...survey, expire_date: ev.target.value})}
-                            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                        />
-                        </div>
-                        {/*Expire Date*/}
-
-                        {/*Active*/}
-                        <div className="flex items-start">
-                        <div className="flex h-5 items-center">
-                            <input
-                            id="status"
-                            name="status"
-                            type="checkbox"
-                            checked={survey.status}
-                            onChange={ev => setSurvey({...survey, status: ev.target.checked})}
-                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                            />
-                        </div>
-                        <div className="ml-3 text-sm">
-                            <label htmlFor="comments" className="font-medium text-gray-700">
-                            Active
+                            {/*Title*/}
+                            <div className="col-span-6 sm:col-span-3">
+                            <label htmlFor="title" className="block text-sm font-medium text-gray-700">
+                                Survey Title
                             </label>
-                            <p className="text-gray-500">Whether to make survey publicly available</p>
-                        </div>
-                        </div>
-                        {/*Active*/}
-                    </div>
+                            <input
+                                type="text"
+                                name="title"
+                                id="title"
+                                value={survey.title}
+                                onChange={ev => setSurvey({...survey, title: ev.target.value})}
+                                placeholder="Survey Title"
+                                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                            />
+                            </div>
+                            {/*Title*/}
 
-                    <button
-                        type="button" 
-                        className="flex items-center text-sm py-1 px-4 mt-2 rounded-sm text-white bg-gray-600 hover:bg-gray-700"
-                        onClick={addQuestion}>
-                        <PlusIcon className="w-4 mr-2"/>
-                        Add question
-                    </button>
-                    <SurveyQuestions questions={survey.questions} onQuestionsUpdate={onQuestionsUpdate} />  
+                            {/*Description*/}
+                            <div className="col-span-6 sm:col-span-3">
+                            <label htmlFor="description" className="block text-sm font-medium text-gray-700">
+                                Description
+                            </label>
+                            <textarea
+                                name="description"
+                                id="description"
+                                value={survey.description || ""}
+                                onChange={ev => setSurvey({...survey, description: ev.target.value})}
+                                placeholder="Describe your survey"
+                                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                            ></textarea>
+                            </div>
+                            {/*Description*/}
 
-                    <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                        <TButton>Save</TButton>
+                            {/*Expire Date*/}
+                            <div className="col-span-6 sm:col-span-3">
+                            <label htmlFor="expire_date" className="block text-sm font-medium text-gray-700">
+                                Expire Date
+                            </label>
+                            <input
+                                type="date"
+                                name="expire_date"
+                                id="expire_date"
+                                value={survey.expire_date}
+                                onChange={ev => setSurvey({...survey, expire_date: ev.target.value})}
+                                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                            />
+                            </div>
+                            {/*Expire Date*/}
+
+                            {/*Active*/}
+                            <div className="flex items-start">
+                            <div className="flex h-5 items-center">
+                                <input
+                                id="status"
+                                name="status"
+                                type="checkbox"
+                                checked={survey.status}
+                                onChange={ev => setSurvey({...survey, status: ev.target.checked})}
+                                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                                />
+                            </div>
+                            <div className="ml-3 text-sm">
+                                <label htmlFor="comments" className="font-medium text-gray-700">
+                                Active
+                                </label>
+                                <p className="text-gray-500">Whether to make survey publicly available</p>
+                            </div>
+                            </div>
+                            {/*Active*/}
+                        </div>
+
+                        <button
+                            type="button" 
+                            className="flex items-center text-sm py-1 px-4 mt-2 rounded-sm text-white bg-gray-600 hover:bg-gray-700"
+                            onClick={addQuestion}>
+                            <PlusIcon className="w-4 mr-2"/>
+                            Add question
+                        </button>
+                        <SurveyQuestions questions={survey.questions} onQuestionsUpdate={onQuestionsUpdate} />  
+
+                        <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
+                            <TButton>Save</TButton>
+                        </div>
                     </div>
-                </div>
-            </form>
+                </form>
+            }
         </PageComponent>
     )
 }

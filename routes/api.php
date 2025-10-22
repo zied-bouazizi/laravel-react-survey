094705c99361138f8c004ea48c266d2b33435fff
Zied Bouazizi <zied.bouazizi@outlook.com>
2025-10-22 10:09:05 +0100

    Implement dashbaord

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\SurveyController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +20,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
     Route::apiResource('survey', SurveyController::class);
+    Route::get('/dashboard', [DashboardController::class, 'index']);
 });
 
 Route::post('/signup', [AuthController::class, 'signup']);
